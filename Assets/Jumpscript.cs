d5b0db5b911263f875585dbed34a9c24a440d6c6
Slowey <lucas.holmqvist@gmail.com>
2017-02-01 16:02:56 +0100

    tjatja, scriptade animationer i unity

---
+++ Assets/Jumpscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpscript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gameObject.transform.position = new Vector3(0,GetComponent<Animator>().GetFloat("JumpHeight"),0);
+        if(transform.position.y > 0.0f)
+        {
+            transform.position -= new Vector3(0, 0.1f, 0);
+        }
+	}
+}
---
+++ Assets/asd.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Director;
+public class asd : MonoBehaviour {
+
+    public AnimationClip m_animClip;
+	// Use this for initialization
+	void Start () {
+        AnimationState state = GetComponent<AnimationState>();
+        state.enabled = true;
+        state.weight = 1;
+        state.normalizedTime = 8.0f / 100;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var clipPlayable = AnimationClipPlayable.Create(m_animClip);
+        GetComponent<Animator>().Play(clipPlayable);
+	}
+}
