339e647cc1e5692d04dedf91e85166b9b3c43972
Slowey <lucas.holmqvist@gmail.com>
2017-02-28 09:32:24 +0100

    tried to fix handbugg

--- Tutorialrawmocap/Assets/Scripts/AnimIdle.cs
+++ Tutorialrawmocap/Assets/Scripts/AnimIdle.cs
@@ -9,6 +9,8 @@ public class AnimIdle : MonoBehaviour {
     int m_IdleIdleHash = Animator.StringToHash("IdleIdle");
     int m_IdleExtendHash = Animator.StringToHash("IdleExtend");
     public float timeAdjuster = 5.733333333333333f;//2.866666666666667f*2.0f;
+    float m_prevTransition = 0.0f;
+    bool m_switch = false;
 
     // Use this for initialization
     public void InitAnim(Animator p_animator)
@@ -33,45 +35,44 @@ public class AnimIdle : MonoBehaviour {
         Transform[] t_bones = t_hips.GetComponentsInChildren<Transform>();
         //print(SQUAD.Spline(poses[0][1], poses[1][1], poses[2][1], poses[3][1], 0.0f).eulerAngles);
         //print(SQUAD.Spline(poses[0][1], poses[1][1], poses[2][1], poses[3][1], 1.0f).eulerAngles);
-
+        
         for (int i = 0; i < t_bones.Length; i++)
         {
-            //if (i == 2 && p_transition > 0.5f && p_prevTrans < 0.5f)
-            //{
-            //    //print(t_bones[i].rotation.eulerAngles);
-            //    //testStruct d = new  testStruct();
-            //    SQUAD.testStruct d = new SQUAD.testStruct();
-            //    //d = SQUAD.Spline(p_poses[0][i], p_poses[1][i], p_poses[0][i], p_poses[1][i], p_prevTrans);
-            //    //print(d.alongLine);
-            //    d = SQUAD.Spline(p_poses[0][i], p_poses[1][i], p_poses[0][i], p_poses[1][i], p_transition);
-            //    t_bones[i].rotation = d.quat;
-            //    print(d.alongLine);
-            //}
-            //else
-            // {
             if (t_bones[i].name.Contains("RightHand"))
             {
-                //Quaternion t_quat = new Quaternion(p_poses[1][i].x * -1, p_poses[1][i].y * -1, p_poses[1][i].z * -1, p_poses[1][i].w * -1);
-                //t_bones[i].rotation = SQUAD.Spline(t_quat, p_poses[0][i], t_quat, p_poses[0][i], p_transition).quat;
-
-                if (p_transition>0.5f)
+                if (p_transition < 0.25f)
                 {
-                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[0][i], p_poses[1][i], p_transition/0.5f);
+                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[0][i], p_poses[1][i], (p_transition - 0.25f) / 0.25f);
                 }
-                else
+                else if(p_transition < 0.5f)
                 {
-                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[1][i], p_poses[0][i], (p_transition -0.5f)/0.5f);
+                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[1][i], p_poses[0][i], (p_transition - 0.5f)/0.25f);
                 }
+                else if(p_transition<0.75f)
+                {
+                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[0][i], p_poses[1][i], (p_transition - 0.75f) / 0.25f);
+                }
+                else if (p_transition <1.0f)
+                {
+                    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[1][i], p_poses[0][i], (p_transition -1.0f)  / 0.25f);
+                }
+                //if (p_transition > 0.5f)
+                //{
+                //    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[0][i], p_poses[1][i], p_transition / 0.5f);
+                //}
+                //else
+                //{
+                //    t_bones[i].rotation = QuaternionExtensionsC.SlerpNoInvertForceShortWay(p_poses[1][i], p_poses[0][i], (p_transition-0.5f) / 0.5f);
+                //}
+                //t_bones[i].rotation = SQUAD.Spline(p_poses[0][i], p_poses[1][i], p_poses[0][i], p_poses[1][i], p_transition).quat;
             }
             else
             {
                 t_bones[i].rotation = SQUAD.Spline(p_poses[1][i], p_poses[0][i], p_poses[1][i], p_poses[0][i], p_transition).quat;
             }
-            //t_bones[0].position = Vector3.Slerp(p_hipspos[0], p_hipspos[1], p_transition);
-             //t_bones[i].rotation = Quaternion.Slerp(p_poses[0][i], p_poses[1][i], p_transition);
-            //}
         }
         //print(p_transition);
+        m_prevTransition = p_transition;
         if (true) 
         {
             if (p_transition < 0.5f)
