d3f5e5c1e3b0fe493621da7eba72e574f2433a7c
Slowey <lucas.holmqvist@gmail.com>
2017-03-08 15:20:20 +0100

    something

--- Tutorialrawmocap/Assets/Scripts/TransitionUpdater.cs
+++ Tutorialrawmocap/Assets/Scripts/TransitionUpdater.cs
@@ -93,6 +93,18 @@ public class TransitionUpdater : MonoBehaviour
                         SaveKeyFramesWalkFWD0();
                         SaveKeyFramesWalkFWD2();
                     }
+                    if (m_framesToAdd == 3)
+                    {
+                        SaveKeyFramesWalkFWD0();
+                        m_between1_1 = Animator.StringToHash("WalkFWD_Extend1_1");
+                        SaveKeyFramesBetween1_1();
+                        m_between1_1 = Animator.StringToHash("WalkFWD_Extend1_1");
+                        SaveKeyFramesBetween1_1();
+                        SaveKeyFramesBetween1_1();
+                        SaveKeyFramesWalkFWD2();
+                    }
+                    break;
+                case AnimationClips.Runnning:
                     break;
                 default:
                     break;
@@ -371,6 +383,24 @@ public class TransitionUpdater : MonoBehaviour
         hipspos.Add(new Vector3(t_hips.transform.position.x, t_hips.transform.position.y, t_hips.transform.position.z));
     }
     void SaveKeyFramesIdleBetween1_1()
+    {
+        GameObject t_hips;
+        List<Transform> t_transforms = new List<Transform>();
+        m_animator.Play(m_between1_1, 0, 0);
+        m_animator.Update(0.001f);
+        m_animator.Stop();
+        t_hips = GameObject.Find("Hips");
+        t_hips.GetComponentsInChildren<Transform>(t_transforms);
+        List<Quaternion> t_quaternions = new List<Quaternion>();
+        for (int i = 0; i < t_transforms.Count; i++)
+        {
+            t_quaternions.Add(new Quaternion(t_transforms[i].rotation.x, t_transforms[i].rotation.y, t_transforms[i].rotation.z,
+                t_transforms[i].rotation.w));
+        }
+        poses.Add(t_quaternions);
+        hipspos.Add(new Vector3(t_hips.transform.position.x, t_hips.transform.position.y, t_hips.transform.position.z));
+    }
+    void SaveKeyFramesBetween1_1()
     {
         GameObject t_hips;
         List<Transform> t_transforms = new List<Transform>();
